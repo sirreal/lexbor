d5f1ffea6c818554d977db5617896205e719cf00
Jon Surrell <sirreal@users.noreply.github.com>
2026-06-09 16:01:35 +0200

    Add SVG xlink href regression tests

--- test/lexbor/html/attributes.c
+++ test/lexbor/html/attributes.c
@@ -14,6 +14,10 @@ const lxb_char_t html[] = "<div id=my-best-id></div>";
 
 const size_t html_length = sizeof(html) - 1;
 
+const lxb_char_t foreign_attrs_html[] = "<svg xlink:href=qual href=plain>";
+
+const size_t foreign_attrs_html_length = sizeof(foreign_attrs_html) - 1;
+
 
 TEST_BEGIN(attrs)
 {
@@ -93,12 +97,98 @@ TEST_BEGIN(attrs)
 }
 TEST_END
 
+TEST_BEGIN(svg_xlink_href_and_href_attrs)
+{
+    lxb_status_t status;
+    lxb_html_body_element_t *body;
+    lxb_html_document_t *document;
+    lxb_dom_collection_t *collection;
+    lxb_dom_element_t *element;
+    lxb_dom_attr_t *attr, *xlink_attr, *href_attr;
+    const lxb_char_t *name;
+    const lxb_char_t *value;
+    size_t len;
+    size_t count;
+
+    document = lxb_html_document_create();
+    test_ne(document, NULL);
+
+    status = lxb_html_document_parse(document, foreign_attrs_html,
+                                     foreign_attrs_html_length);
+    test_eq(status, LXB_STATUS_OK);
+
+    collection = lxb_dom_collection_make(&document->dom_document, 16);
+    test_ne(collection, NULL);
+
+    body = lxb_html_document_body_element(document);
+    element = lxb_dom_interface_element(body);
+
+    status = lxb_dom_elements_by_tag_name(element, collection,
+                                          (const lxb_char_t *) "svg", 3);
+
+    test_eq(status, LXB_STATUS_OK);
+    test_eq(lxb_dom_collection_length(collection), 1);
+
+    element = lxb_dom_collection_element(collection, 0);
+
+    count = 0;
+    xlink_attr = NULL;
+    href_attr = NULL;
+    attr = lxb_dom_element_first_attribute(element);
+
+    while (attr != NULL) {
+        count++;
+
+        name = lxb_dom_attr_qualified_name(attr, &len);
+
+        if (len == 10
+            && lexbor_str_data_ncmp(name, (const lxb_char_t *) "xlink:href",
+                                    len))
+        {
+            xlink_attr = attr;
+        }
+        else if (len == 4
+                 && lexbor_str_data_ncmp(name, (const lxb_char_t *) "href",
+                                         len))
+        {
+            href_attr = attr;
+        }
+
+        attr = lxb_dom_element_next_attribute(attr);
+    }
+
+    test_eq(count, 2);
+    test_ne(xlink_attr, NULL);
+    test_ne(href_attr, NULL);
+    test_ne(xlink_attr, href_attr);
+
+    test_eq(xlink_attr->node.ns, LXB_NS_XLINK);
+    test_eq(href_attr->node.ns, LXB_NS_SVG);
+
+    name = lxb_dom_attr_local_name(xlink_attr, &len);
+    test_eq_str_n(name, len, (const lxb_char_t *) "href", 4);
+
+    name = lxb_dom_attr_local_name(href_attr, &len);
+    test_eq_str_n(name, len, (const lxb_char_t *) "href", 4);
+
+    value = lxb_dom_attr_value(xlink_attr, &len);
+    test_eq_str_n(value, len, (const lxb_char_t *) "qual", 4);
+
+    value = lxb_dom_attr_value(href_attr, &len);
+    test_eq_str_n(value, len, (const lxb_char_t *) "plain", 5);
+
+    lxb_dom_collection_destroy(collection, true);
+    lxb_html_document_destroy(document);
+}
+TEST_END
+
 int
 main(int argc, const char * argv[])
 {
     TEST_INIT();
 
     TEST_ADD(attrs);
+    TEST_ADD(svg_xlink_href_and_href_attrs);
 
     TEST_RUN("lexbor/html/attributes");
     TEST_RELEASE();
